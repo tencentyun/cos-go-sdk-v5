0ac679c118578cf53a9c273647a7d39e209d4ce9
toranger <toranger@outlook.com>
2019-02-26 22:00:39 +0800

    Delete the ak and sk info

--- example/object/restore.go
+++ example/object/restore.go
@@ -2,30 +2,23 @@ package main
 
 import (
 	"context"
-	// "os"
+	"os"
 
 	"net/url"
 
 	"net/http"
 
-	// "github.com/tencentyun/cos-go-sdk-v5"
-	// "github.com/tencentyun/cos-go-sdk-v5/debug"
-	"github.com/toranger/cos-go-sdk-v5"
-	"github.com/toranger/cos-go-sdk-v5/debug"
+	"github.com/tencentyun/cos-go-sdk-v5"
+	"github.com/tencentyun/cos-go-sdk-v5/debug"
 )
 
 func main() {
-	ak := "AKID4ygEetn1tZ6UingT44tU5smniXNEthIo"
-	sk := "gHnDWp7NuLlBVmxAoRyPl0PoLrQqBMQK"
-	u, _ := url.Parse("https://alangz-1253960454.cos.ap-guangzhou.myqcloud.com")
-	// u, _ := url.Parse("https://test-1253846586.cos.ap-guangzhou.myqcloud.com")
+	u, _ := url.Parse("https://test-1253846586.cos.ap-guangzhou.myqcloud.com")
 	b := &cos.BaseURL{BucketURL: u}
 	c := cos.NewClient(b, &http.Client{
 		Transport: &cos.AuthorizationTransport{
-			// SecretID:  os.Getenv("COS_SECRETID"),
-			// SecretKey: os.Getenv("COS_SECRETKEY"),
-			SecretID:  ak,
-			SecretKey: sk,
+			SecretID:  os.Getenv("COS_SECRETID"),
+			SecretKey: os.Getenv("COS_SECRETKEY"),
 			Transport: &debug.DebugRequestTransport{
 				RequestHeader:  true,
 				RequestBody:    true,
