63b6872acd7bb275f42af18120b16b6d7776dc87
jojoliang <agin719@126.com>
2020-04-12 17:28:28 +0800

    add encryption and referer

---
+++ bucket_encryption_test.go
@@ -0,0 +1,100 @@
+package cos
+
+import (
+	"context"
+	"encoding/xml"
+	"fmt"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestBucketService_GetEncryption(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		vs := values{
+			"encryption": "",
+		}
+		testFormValues(t, r, vs)
+		fmt.Fprint(w, `<ServerSideEncryptionConfiguration>
+    <Rule>
+        <ApplySideEncryptionConfiguration>
+            <SSEAlgorithm>AES256</SSEAlgorithm>
+        </ApplySideEncryptionConfiguration>
+    </Rule>
+</ServerSideEncryptionConfiguration>`)
+
+	})
+
+	res, _, err := client.Bucket.GetEncryption(context.Background())
+	if err != nil {
+		t.Fatalf("Bucket.GetEncryption returned error %v", err)
+	}
+
+	want := &BucketGetEncryptionResult{
+		XMLName: xml.Name{Local: "ServerSideEncryptionConfiguration"},
+		Rule: &BucketEncryptionConfiguration{
+			SSEAlgorithm: "AES256",
+		},
+	}
+
+	if !reflect.DeepEqual(res, want) {
+		t.Errorf("Bucket.GetEncryption returned %+v, want %+v", res, want)
+	}
+}
+
+func TestBucketService_PutEncryption(t *testing.T) {
+	setup()
+	defer teardown()
+
+	opt := &BucketPutEncryptionOptions{
+		Rule: &BucketEncryptionConfiguration{
+			SSEAlgorithm: "AES256",
+		},
+	}
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "PUT")
+		vs := values{
+			"encryption": "",
+		}
+		testFormValues(t, r, vs)
+
+		body := new(BucketPutEncryptionOptions)
+		xml.NewDecoder(r.Body).Decode(body)
+		want := opt
+		want.XMLName = xml.Name{Local: "ServerSideEncryptionConfiguration"}
+		if !reflect.DeepEqual(body, want) {
+			t.Errorf("Bucket.PutEncryption request\n body: %+v\n, want %+v\n", body, want)
+		}
+	})
+
+	_, err := client.Bucket.PutEncryption(context.Background(), opt)
+	if err != nil {
+		t.Fatalf("Bucket.PutEncryption returned error: %v", err)
+	}
+}
+
+func TestBucketService_DeleteEncryption(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodDelete)
+		vs := values{
+			"encryption": "",
+		}
+		testFormValues(t, r, vs)
+
+		w.WriteHeader(http.StatusNoContent)
+	})
+
+	_, err := client.Bucket.DeleteEncryption(context.Background())
+	if err != nil {
+		t.Fatalf("Bucket.DeleteEncryption returned error: %v", err)
+	}
+
+}
---
+++ example/bucket/referer.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/tencentyun/cos-go-sdk-v5"
+	"github.com/tencentyun/cos-go-sdk-v5/debug"
+)
+
+func log_status(err error) {
+	if err == nil {
+		return
+	}
+	if cos.IsNotFoundError(err) {
+		// WARN
+		fmt.Println("Resource is not existed")
+	} else if e, ok := cos.IsCOSError(err); ok {
+		fmt.Printf("Code: %v\n", e.Code)
+		fmt.Printf("Message: %v\n", e.Message)
+		fmt.Printf("Resource: %v\n", e.Resource)
+		fmt.Printf("RequestId: %v\n", e.RequestID)
+		// ERROR
+	} else {
+		fmt.Println(err)
+		// ERROR
+	}
+}
+
+func main() {
+	u, _ := url.Parse("https://test-1259654469.cos.ap-guangzhou.myqcloud.com")
+	b := &cos.BaseURL{
+		BucketURL: u,
+	}
+	c := cos.NewClient(b, &http.Client{
+		Transport: &cos.AuthorizationTransport{
+			SecretID:  os.Getenv("COS_SECRETID"),
+			SecretKey: os.Getenv("COS_SECRETKEY"),
+			Transport: &debug.DebugRequestTransport{
+				RequestHeader:  true,
+				RequestBody:    true,
+				ResponseHeader: true,
+				ResponseBody:   true,
+			},
+		},
+	})
+
+	opt := &cos.BucketPutRefererOptions{
+		Status:      "Enabled",
+		RefererType: "White-List",
+		DomainList: []string{
+			"*.qq.com",
+			"*.qcloud.com",
+		},
+		EmptyReferConfiguration: "Allow",
+	}
+
+	_, err := c.Bucket.PutReferer(context.Background(), opt)
+	log_status(err)
+
+	res, _, err := c.Bucket.GetReferer(context.Background())
+	log_status(err)
+	fmt.Printf("%+v\n", res)
+}
