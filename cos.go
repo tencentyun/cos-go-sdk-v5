152b0634ca147d5695644361eaa966e81e875e73
jojoliang <agin719@126.com>
2021-06-25 16:46:53 +0800

    update to v0.7.27

--- cos.go
+++ cos.go
@@ -22,7 +22,7 @@ import (
 
 const (
 	// Version current go sdk version
-	Version               = "0.7.26"
+	Version               = "0.7.27"
 	userAgent             = "cos-go-sdk-v5/" + Version
 	contentTypeXML        = "application/xml"
 	defaultServiceBaseURL = "http://service.cos.myqcloud.com"
