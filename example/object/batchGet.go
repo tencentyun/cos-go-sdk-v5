4c068949a6a9b71c8e8b282be606e62bc1a8533f
jojoliang <agin719@126.com>
2021-11-29 15:48:21 +0800

    update PresignedURL && add batchGet demo

--- example/object/batchGet.go
+++ example/object/batchGet.go
@@ -6,7 +6,8 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"path/filepath"
+	"path"
+	"strings"
 	"sync"
 
 	"github.com/tencentyun/cos-go-sdk-v5"
@@ -32,11 +33,11 @@ func log_status(err error) {
 	}
 }
 
-func upload(wg *sync.WaitGroup, c *cos.Client, keysCh <-chan string) {
+func download(wg *sync.WaitGroup, c *cos.Client, keysCh <-chan []string) {
 	defer wg.Done()
-	for key := range keysCh {
-		// 下载文件到当前目录
-		_, filename := filepath.Split(key)
+	for keys := range keysCh {
+		key := keys[0]
+		filename := keys[1]
 		_, err := c.Object.GetToFile(context.Background(), key, filename, nil)
 		if err != nil {
 			log_status(err)
@@ -51,24 +52,52 @@ func main() {
 			SecretID:  os.Getenv("COS_SECRETID"),
 			SecretKey: os.Getenv("COS_SECRETKEY"),
 			Transport: &debug.DebugRequestTransport{
-				RequestHeader: true,
+				RequestHeader: false,
 				// Notice when put a large file and set need the request body, might happend out of memory error.
 				RequestBody:    false,
-				ResponseHeader: true,
+				ResponseHeader: false,
 				ResponseBody:   false,
 			},
 		},
 	})
-	keysCh := make(chan string, 2)
-	keys := []string{"test/test1", "test/test2", "test/test3"}
+	// 多线程执行
+	keysCh := make(chan []string, 3)
 	var wg sync.WaitGroup
-	threadpool := 2
+	threadpool := 3
 	for i := 0; i < threadpool; i++ {
 		wg.Add(1)
-		go upload(&wg, c, keysCh)
+		go download(&wg, c, keysCh)
 	}
-	for _, key := range keys {
-		keysCh <- key
+	isTruncated := true
+	prefix := "dir" // 下载 dir 目录下所有文件
+	marker := ""
+	localDir := "./local/"
+	for isTruncated {
+		opt := &cos.BucketGetOptions{
+			Prefix:       prefix,
+			Marker:       marker,
+			EncodingType: "url", // url编码
+		}
+		// 列出目录
+		v, _, err := c.Bucket.Get(context.Background(), opt)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		for _, c := range v.Contents {
+			key, _ := cos.DecodeURIComponent(c.Key) //EncodingType: "url"，先对 key 进行 url decode
+			localfile := localDir + key
+			if _, err := os.Stat(path.Dir(localfile)); err != nil && os.IsNotExist(err) {
+				os.MkdirAll(path.Dir(localfile), os.ModePerm)
+			}
+			// 目录不需要下载
+			if strings.HasSuffix(localfile, "/") {
+				continue
+			}
+			keysCh <- []string{key, localfile}
+		}
+		marker = v.NextMarker
+		isTruncated = v.IsTruncated
 	}
 	close(keysCh)
 	wg.Wait()
