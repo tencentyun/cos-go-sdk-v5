5389852aef138d8bc70dba4ccd90d8f5b2ec733f
jojoliang <agin719@126.com>
2022-10-12 11:17:29 +0800

    add delete bucket opt

--- bucket.go
+++ bucket.go
@@ -84,14 +84,23 @@ func (s *BucketService) Put(ctx context.Context, opt *BucketPutOptions) (*Respon
 	return resp, err
 }
 
+type BucketDeleteOptions struct {
+	XOptionHeader *http.Header `header:"-,omitempty" url:"-" xml:"-"`
+}
+
 // Delete Bucket请求可以在指定账号下删除Bucket，删除之前要求Bucket为空。
 //
 // https://www.qcloud.com/document/product/436/7732
-func (s *BucketService) Delete(ctx context.Context) (*Response, error) {
+func (s *BucketService) Delete(ctx context.Context, opt ...*BucketDeleteOptions) (*Response, error) {
+	var dopt *BucketDeleteOptions
+	if len(opt) > 0 {
+		dopt = opt[0]
+	}
 	sendOpt := sendOptions{
-		baseURL: s.client.BaseURL.BucketURL,
-		uri:     "/",
-		method:  http.MethodDelete,
+		baseURL:   s.client.BaseURL.BucketURL,
+		uri:       "/",
+		method:    http.MethodDelete,
+		optHeader: dopt,
 	}
 	resp, err := s.client.send(ctx, &sendOpt)
 	return resp, err
@@ -103,9 +112,9 @@ type BucketHeadOptions struct {
 
 // Head Bucket请求可以确认是否存在该Bucket，是否有权限访问，Head的权限与Read一致。
 //
-//   当其存在时，返回 HTTP 状态码200；
-//   当无权限时，返回 HTTP 状态码403；
-//   当不存在时，返回 HTTP 状态码404。
+//	当其存在时，返回 HTTP 状态码200；
+//	当无权限时，返回 HTTP 状态码403；
+//	当不存在时，返回 HTTP 状态码404。
 //
 // https://www.qcloud.com/document/product/436/7735
 func (s *BucketService) Head(ctx context.Context, opt ...*BucketHeadOptions) (*Response, error) {
