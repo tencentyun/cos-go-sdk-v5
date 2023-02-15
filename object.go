bd58b1fe954759fe0847b82dd859a386467c95d9
jojoliang <agin719@126.com>
2023-02-15 19:45:48 +0800

    update Object Info

--- object.go
+++ object.go
@@ -676,14 +676,16 @@ func (s *ObjectService) DeleteMulti(ctx context.Context, opt *ObjectDeleteMultiO
 
 // Object is the meta info of the object
 type Object struct {
-	Key          string `xml:",omitempty"`
-	ETag         string `xml:",omitempty"`
-	Size         int64  `xml:",omitempty"`
-	PartNumber   int    `xml:",omitempty"`
-	LastModified string `xml:",omitempty"`
-	StorageClass string `xml:",omitempty"`
-	Owner        *Owner `xml:",omitempty"`
-	VersionId    string `xml:",omitempty"`
+	Key           string `xml:",omitempty"`
+	ETag          string `xml:",omitempty"`
+	Size          int64  `xml:",omitempty"`
+	PartNumber    int    `xml:",omitempty"`
+	LastModified  string `xml:",omitempty"`
+	StorageClass  string `xml:",omitempty"`
+	Owner         *Owner `xml:",omitempty"`
+	VersionId     string `xml:",omitempty"`
+	StorageTier   string `xml:",omitempty"`
+	RestoreStatus string `xml:",omitempty"`
 }
 
 // MultiUploadOptions is the option of the multiupload,
@@ -1014,7 +1016,6 @@ func (s *ObjectService) checkUploadedParts(ctx context.Context, name, UploadID, 
 //
 // 当 partSize > 0 时，由调用者指定分块大小，否则由 SDK 自动切分，单位为MB
 // 由调用者指定分块大小时，请确认分块数量不超过10000
-//
 func (s *ObjectService) MultiUpload(ctx context.Context, name string, filepath string, opt *MultiUploadOptions) (*CompleteMultipartUploadResult, *Response, error) {
 	return s.Upload(ctx, name, filepath, opt)
 }
