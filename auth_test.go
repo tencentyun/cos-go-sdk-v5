808cb0447724a20cb081055bf0012db9baf1e2af
jojoliang <agin719@126.com>
2024-07-11 17:49:39 +0800

    download分块可以超过10000, 增加sts签名方式

--- auth_test.go
+++ auth_test.go
@@ -235,7 +235,7 @@ func TestCVMCredentialTransportErr(t *testing.T) {
 		secretID:     "ak",
 		secretKey:    "sk",
 		sessionToken: "token",
-		expiredTime:  nt + defaultCVMAuthExpire + 1,
+		expiredTime:  nt + defaultTmpAuthExpire + 1,
 	}
 	// 密钥未超时
 	ak, sk, token, err := transport.UpdateCredential(nt)
@@ -243,7 +243,7 @@ func TestCVMCredentialTransportErr(t *testing.T) {
 		t.Errorf("UpdateCredential failed, return: %v, %v, %v, want: %v", ak, sk, token, *transport)
 	}
 	// 密钥超时，GetRoles返回错误
-	transport.expiredTime = nt + defaultCVMAuthExpire - 1
+	transport.expiredTime = nt + defaultTmpAuthExpire - 1
 	ak, sk, token, err = transport.UpdateCredential(nt)
 	if ak != transport.secretID || sk != transport.secretKey || token != transport.sessionToken || err == nil {
 		t.Errorf("UpdateCredential failed, return: %v, %v, %v, want: %v", ak, sk, token, *transport)
@@ -316,4 +316,172 @@ func TestCredentialTransport(t *testing.T) {
 	client.doAPI(context.Background(), req, nil, true)
 	client.GetCredential()
 
+}
+
+func TestStsCredentialTransport(t *testing.T) {
+	setup()
+	defer teardown()
+	uri := client.BaseURL.BucketURL.String()
+	ak := "test_ak"
+	sk := "test_sk"
+	token := "test_token"
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("x-cos-security-token") != token {
+			t.Errorf("StsCredentialTransport x-cos-security-token error, want:%v, return:%v\n", token, r.Header.Get("x-cos-security-token"))
+		}
+		auth := r.Header.Get("Authorization")
+		if auth == "" {
+			t.Error("StsCredentialTransport didn't add Authorization header")
+		}
+		field := strings.Split(auth, "&")
+		if len(field) != 7 {
+			t.Errorf("StsCredentialTransport Authorization header format error: %v\n", auth)
+		}
+		st_et := strings.Split(strings.Split(field[2], "=")[1], ";")
+		st, _ := strconv.ParseInt(st_et[0], 10, 64)
+		et, _ := strconv.ParseInt(st_et[1], 10, 64)
+		authTime := &AuthTime{
+			SignStartTime: time.Unix(st, 0),
+			SignEndTime:   time.Unix(et, 0),
+			KeyStartTime:  time.Unix(st, 0),
+			KeyEndTime:    time.Unix(et, 0),
+		}
+		host := strings.TrimLeft(uri, "http://")
+		req, _ := http.NewRequest("GET", uri, nil)
+		req.Header.Add("Host", host)
+		expect := newAuthorization(ak, sk, req, authTime, true)
+		if expect != auth {
+			t.Errorf("StsCredentialTransport Authorization error, want:%v, return:%v\n", expect, auth)
+		}
+	})
+
+	// CVM http server
+	cvm_mux := http.NewServeMux()
+	cvm_server := httptest.NewServer(cvm_mux)
+	defer cvm_server.Close()
+	// 将默认 CVM Host 修改成测试IP:PORT
+	defaultStsSchema = "http"
+	defaultStsHost = strings.TrimLeft(cvm_server.URL, "http://")
+
+	cvm_mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, fmt.Sprintf(`{
+			"Response": {
+				"Credentials": {
+					"TmpSecretId": "%v",
+					"TmpSecretKey": "%v",
+					"Token": "%v"
+				},
+				"Expiration": "2023-06-14T05:06:57Z",
+				"ExpiredTime": 1686719217,
+				"RequestId": "59a5e07e-4147-4d2e-a808-dca76ac5b3fd"
+			}
+		}`, ak, sk, token))
+	})
+
+	client.client.Transport = &StsCredentialTransport{}
+	req, _ := http.NewRequest("GET", client.BaseURL.BucketURL.String(), nil)
+	_, err := client.doAPI(context.Background(), req, nil, true)
+	if err != nil {
+		t.Errorf("doAPI failed: %v", err)
+	}
+
+	_, err = client.doAPI(context.Background(), req, nil, true)
+	if err != nil {
+		t.Errorf("doAPI failed: %v", err)
+	}
+
+	client.client.Transport = &StsCredentialTransport{
+		Policy: &CredentialPolicy{
+			Statement: []CredentialPolicyStatement{
+				{
+					// 密钥的权限列表。简单上传和分片需要以下的权限，其他权限列表请看 https://cloud.tencent.com/document/product/436/31923
+					Action: []string{
+						// 简单上传
+						"name/cos:GetObject",
+					},
+					Effect: "allow",
+					Resource: []string{
+						// 这里改成允许的路径前缀，可以根据自己网站的用户登录态判断允许上传的具体路径，例子： a.jpg 或者 a/* 或者 * (使用通配符*存在重大安全风险, 请谨慎评估使用)
+						// 存储桶的命名格式为 BucketName-APPID，此处填写的 bucket 必须为此格式
+						"qcs::cos:ap-guangzhou:uid/1250000000:test-12500000000/*",
+					},
+				},
+			},
+		},
+		Host: strings.TrimLeft(cvm_server.URL, "http://"),
+	}
+	req, _ = http.NewRequest("GET", client.BaseURL.BucketURL.String(), nil)
+	_, err = client.doAPI(context.Background(), req, nil, true)
+	if err != nil {
+		t.Errorf("doAPI failed: %v", err)
+	}
+
+}
+
+func TestStsCredentialTransportErr(t *testing.T) {
+	setup()
+	defer teardown()
+
+	// CVM http server
+	cvm_mux := http.NewServeMux()
+	cvm_server := httptest.NewServer(cvm_mux)
+	defer cvm_server.Close()
+	// 将默认 CVM Host 修改成测试IP:PORT
+	defaultStsSchema = "http"
+
+	var expectErr int
+	cvm_mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if expectErr == 0 {
+			w.WriteHeader(http.StatusGatewayTimeout)
+		} else if expectErr == 1 {
+			fmt.Fprint(w, `{"RequestId": "59a5e07e-4147-4d2e-a808-dca76ac5b3fd",}`)
+		} else if expectErr == 2 {
+			fmt.Fprint(w, `{"Response": {
+				"Error": {
+					"Code": "error",
+					"Message": "error"
+				},
+				"RequestId": "59a5e07e-4147-4d2e-a808-dca76ac5b3fd"
+			}}`)
+		} else if expectErr == 3 {
+			fmt.Fprint(w, `{"Response": {
+				"Expiration": "2023-06-14T05:06:57Z",
+				"ExpiredTime": 1686719217,
+				"RequestId": "59a5e07e-4147-4d2e-a808-dca76ac5b3fd"
+			}}`)
+		}
+	})
+
+	client.client.Transport = &StsCredentialTransport{
+		Host: strings.TrimLeft(cvm_server.URL, "http://"),
+	}
+	req, _ := http.NewRequest("GET", client.BaseURL.BucketURL.String(), nil)
+	expectErr = 0
+	_, err := client.doAPI(context.Background(), req, nil, true)
+	if err == nil {
+		t.Errorf("doAPI expect error")
+	}
+	expectErr = 1
+	_, err = client.doAPI(context.Background(), req, nil, true)
+	if err == nil {
+		t.Errorf("doAPI expect error")
+	}
+	expectErr = 2
+	_, err = client.doAPI(context.Background(), req, nil, true)
+	if err == nil {
+		t.Errorf("doAPI expect error")
+	}
+	expectErr = 3
+	_, err = client.doAPI(context.Background(), req, nil, true)
+	if err == nil {
+		t.Errorf("doAPI expect error")
+	}
+	if (&CredentialError{
+		Code:      "error",
+		Message:   "error",
+		RequestId: "error",
+	}).Error() != "Code: error, Message: error, RequestId: error" {
+		t.Errorf("CredentialError format error")
+	}
 }
---
+++ example/object/get_with_sts.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/tencentyun/cos-go-sdk-v5"
+	"github.com/tencentyun/cos-go-sdk-v5/debug"
+)
+
+func log_status(err error) {
+	if err == nil {
+		return
+	}
+	if cos.IsNotFoundError(err) {
+		// WARN
+		fmt.Println("WARN: Resource is not existed")
+	} else if e, ok := cos.IsCOSError(err); ok {
+		fmt.Printf("ERROR: Code: %v\n", e.Code)
+		fmt.Printf("ERROR: Message: %v\n", e.Message)
+		fmt.Printf("ERROR: Resource: %v\n", e.Resource)
+		fmt.Printf("ERROR: RequestId: %v\n", e.RequestID)
+		// ERROR
+	} else {
+		fmt.Printf("ERROR: %v\n", err)
+		// ERROR
+	}
+}
+
+func main() {
+	// 存储桶名称，由bucketname-appid 组成，appid必须填入，可以在COS控制台查看存储桶名称。 https://console.cloud.tencent.com/cos5/bucket
+	// 替换为用户的 region，存储桶region可以在COS控制台“存储桶概览”查看 https://console.cloud.tencent.com/ ，关于地域的详情见 https://cloud.tencent.com/document/product/436/6224 。
+	u, _ := url.Parse("https://test-1259654469.cos.ap-guangzhou.myqcloud.com")
+	b := &cos.BaseURL{BucketURL: u}
+	c := cos.NewClient(b, &http.Client{
+		Transport: &cos.StsCredentialTransport{
+			SecretID:  os.Getenv("COS_SECRETID"),
+			SecretKey: os.Getenv("COS_SECRETKEY"),
+			Transport: &debug.DebugRequestTransport{
+				RequestHeader:  true,
+				RequestBody:    true,
+				ResponseHeader: true,
+				ResponseBody:   false,
+			},
+			Policy: &cos.CredentialPolicy{
+				Statement: []cos.CredentialPolicyStatement{
+					{
+						// 密钥的权限列表。简单上传和分片需要以下的权限，其他权限列表请看 https://cloud.tencent.com/document/product/436/31923
+						Action: []string{
+							// 简单上传
+							"name/cos:GetObject",
+						},
+						Effect: "allow",
+						Resource: []string{
+							// 这里改成允许的路径前缀，可以根据自己网站的用户登录态判断允许上传的具体路径，例子： a.jpg 或者 a/* 或者 * (使用通配符*存在重大安全风险, 请谨慎评估使用)
+							// 存储桶的命名格式为 BucketName-APPID，此处填写的 bucket 必须为此格式
+							"qcs::cos:ap-guangzhou:uid/1259654469:test-1259654469/example",
+						},
+					},
+				},
+			},
+		},
+	})
+
+	// Case1 上传对象
+	name := "example"
+	// Case3 通过本地文件上传对象
+	_, err := c.Object.GetToFile(context.Background(), name, "./test", nil) // 请求的超时时间为 min{context超时时间， HTTP超时时间}
+	log_status(err)
+}
