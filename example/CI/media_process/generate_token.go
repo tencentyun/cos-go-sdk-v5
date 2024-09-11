26d3550b9c425e2f3ff3a019d8d5f2906f2e2bfe
wanjiewu <wanjiewu@tencent.com>
2024-09-11 12:12:05 +0800

    解决jwt库的版本安全问题

--- example/CI/media_process/generate_token.go
+++ example/CI/media_process/generate_token.go
@@ -8,7 +8,7 @@ import (
 	"os"
 	"time"
 
-	"github.com/dgrijalva/jwt-go"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/tencentyun/cos-go-sdk-v5"
 )
 
@@ -16,57 +16,33 @@ type URLToken struct {
 	SessionToken string `url:"x-cos-security-token,omitempty" header:"-"`
 }
 
-type JwtTokens struct {
-	// base info
-	Type     string `json:"Type"`
-	AppId    string `json:"AppId"`
-	BucketId string `json:"BucketId"`
-	Object   string `json:"Object"`
-	Issuer   string `json:"Issuer"`
-	// time info
-	IssuedTimeStamp int64 `json:"IssuedTimeStamp"`
-	ExpireTimeStamp int64 `json:"ExpireTimeStamp"`
-	// other info
-	Random int64 `json:"Random"`
-	// times info
-	UsageLimit int `json:"UsageLimit"`
-	// secret info
-	ProtectSchema     string `json:"ProtectSchema"`
-	PublicKey         string `json:"PublicKey"`
-	ProtectContentKey int    `json:"ProtectContentKey"`
-}
-
-func (token JwtTokens) Valid() error {
-	return nil
-}
-
 // 生成jwt
 func GenerateToken(appId string, bucketId string, objectKey string, secret []byte) (string, error) {
 	t := time.Now()
 	now := t.Unix()
-	payLoad := JwtTokens{
+	payLoad := jwt.MapClaims{
 		// 固定为 CosCiToken， 必填参数
-		Type: "CosCiToken",
+		"Type": "CosCiToken",
 		// app id，必填参数
-		AppId: appId,
+		"AppId": appId,
 		// 播放文件所在的BucketId， 必填参数
-		BucketId: bucketId,
+		"BucketId": bucketId,
 		// 播放文件名
-		Object: url.QueryEscape(objectKey),
+		"Object": url.QueryEscape(objectKey),
 		// 固定为client，必填参数
-		Issuer: "client",
+		"Issuer": "client",
 		// token颁发时间戳，必填参数
-		IssuedTimeStamp: now,
+		"IssuedTimeStamp": now,
 		// token过期时间戳，非必填参数，默认1天过期
-		ExpireTimeStamp: t.Add(time.Hour * 24 * 6).Unix(),
+		"ExpireTimeStamp": t.Add(time.Hour * 24 * 6).Unix(),
 		// token使用次数限制，非必填参数，默认限制100次
-		UsageLimit: 20,
+		"UsageLimit": 20,
 		// 保护模式，填写为 rsa1024 ，则表示使用 RSA 非对称加密的方式保护，公私钥对长度为 1024 bit
-		ProtectSchema: "rsa1024",
+		"ProtectSchema": "rsa1024",
 		// 公钥。1024 bit 的 RSA 公钥，需使用 Base64 进行编码
-		PublicKey: "xxx",
+		"PublicKey": "xxx",
 		// 是否加密解密密钥（播放时解密ts视频流的密钥），1表示对解密密钥加密，0表示不对解密密钥加密。
-		ProtectContentKey: 0,
+		"ProtectContentKey": 0,
 	}
 	//使用指定的签名方法创建签名对象
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, payLoad)
