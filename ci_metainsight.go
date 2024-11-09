2ee39750388eee7a5cd0e50cfb861487e002c149
branshchen <branshchen@tencent.com>
2024-11-09 16:50:29 +0800

    update CreateDataset

--- ci_metainsight.go
+++ ci_metainsight.go
@@ -57,10 +57,15 @@ func (s *MetaInsightService) baseSend(ctx context.Context, opt interface{}, opti
 }
 
 type CreateDatasetOptions struct {
-	DatasetName string      `json:"DatasetName, omitempty" url:"-" ` // 数据集名称，同一个账户下唯一。命名规则如下： 长度为1~32字符。 只能包含小写英文字母，数字，短划线（-）。 必须以英文字母和数字开头。
-	Description string      `json:"Description, omitempty" url:"-" ` // 数据集描述信息。长度为1~256个英文或中文字符，默认值为空。
-	TemplateId  string      `json:"TemplateId, omitempty" url:"-" `  // 指模板，在建立元数据索引时，后端将根据模板来决定收集哪些元数据。每个模板都包含一个或多个算子，不同的算子表示不同的元数据。目前支持的模板： Official:DefaultEmptyId：默认为空的模板，表示不进行元数据的采集。 Official:COSBasicMeta：基础信息模板，包含 COS 文件基础元信息算子，表示采集 COS 文件的名称、类型、ACL等基础元信息数据。 Official:FaceSearch：人脸检索模板，包含人脸检索、COS 文件基础元信息算子。Official:ImageSearch：图像检索模板，包含图像检索、COS 文件基础元信息算子。
-	OptHeaders  *OptHeaders `header:"-, omitempty" url:"-" json:"-" xml:"-"`
+	DatasetName     string      `json:"DatasetName, omitempty" url:"-" `     // 数据集名称，同一个账户下唯一。命名规则如下： 长度为1~32字符。 只能包含小写英文字母，数字，短划线（-）。 必须以英文字母和数字开头。
+	Description     string      `json:"Description, omitempty" url:"-" `     // 数据集描述信息。长度为1~256个英文或中文字符，默认值为空。
+	TemplateId      string      `json:"TemplateId, omitempty" url:"-" `      // 指模板，在建立元数据索引时，后端将根据模板来决定收集哪些元数据。每个模板都包含一个或多个算子，不同的算子表示不同的元数据。目前支持的模板： Official:DefaultEmptyId：默认为空的模板，表示不进行元数据的采集。 Official:COSBasicMeta：基础信息模板，包含 COS 文件基础元信息算子，表示采集 COS 文件的名称、类型、ACL等基础元信息数据。 Official:FaceSearch：人脸检索模板，包含人脸检索、COS 文件基础元信息算子。Official:ImageSearch：图像检索模板，包含图像检索、COS 文件基础元信息算子。
+	Version         string      `json:"Version, omitempty" url:"-" `         // 数据集版本。basic、standard，默认为basic。
+	Volume          int         `json:"Volume, omitempty" url:"-" `          // Version为basic时为50w。Version为standard时，默认为500w，可设置1-10000，单位w。传0采用默认值。
+	TrainingMode    int         `json:"TrainingMode, omitempty" url:"-" `    // 训练数据的来源模式。默认为0，表示训练数据来源于指定数据集，值为1时表示训练数据来源于cos某个bucket目录下文件。仅在Version为standard时生效。
+	TrainingDataset string      `json:"TrainingDataset, omitempty" url:"-" ` // 训练数据的数据集名称。仅在TrainingMode为0时生效。
+	TrainingURI     string      `json:"TrainingURI, omitempty" url:"-" `     // 训练数据的资源路径。仅在TrainingMode为1时生效。
+	OptHeaders      *OptHeaders `header:"-, omitempty" url:"-" json:"-" xml:"-"`
 }
 
 type CreateDatasetResult struct {
