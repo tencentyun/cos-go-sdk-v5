a05da0695a5436fbfe960bafef45a01d5e3d4a4a
jojoliang <agin719@126.com>
2025-02-10 20:58:00 +0800

    将x-cos-security-token签入到签名

--- cos.go
+++ cos.go
@@ -26,7 +26,7 @@ import (
 
 const (
 	// Version current go sdk version
-	Version               = "0.7.61"
+	Version               = "0.7.62"
 	UserAgent             = "cos-go-sdk-v5/" + Version
 	contentTypeXML        = "application/xml"
 	defaultServiceBaseURL = "http://service.cos.myqcloud.com"
